32a2cc1c406a277740dc557bb9a13b0126031797
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-18 01:29:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,7 +5,7 @@
 
 def display_rainbow(array)
   
-puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+puts "R: #{array[0]}, O: #{array[1]}, Y: #{array[2]}, G: #{array[3]}, B: #{array[4]}, I: #{array[5]}, V: #{array[6]}"
 
 
 end
