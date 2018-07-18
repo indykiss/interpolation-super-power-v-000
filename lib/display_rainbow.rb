fdf9d750e20132c159d7c51ce2c3878c998c796c
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-18 01:25:27 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -9,8 +9,6 @@ puts "#{array[0][0]}.uppercase, #{array[1][0]}.uppercase,#{array[2][0]}.uppercas
 #{array[3][0]}.uppercase, #{array[4][0]}.uppercase, #{array[5][0]}.uppercase,
 #{array[6][0]}.uppercase, #{array[7][0]}.uppercase"
 
-
-  
 end
 
 display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
