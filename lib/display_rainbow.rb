9216f2f9ebd2b2399d320af0ffc2d79f092b3c1b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-18 01:26:37 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,9 +5,8 @@
 
 def display_rainbow(array)
   
-puts "#{array[0][0]}.uppercase, #{array[1][0]}.uppercase,#{array[2][0]}.uppercase
-#{array[3][0]}.uppercase, #{array[4][0]}.uppercase, #{array[5][0]}.uppercase,
-#{array[6][0]}.uppercase, #{array[7][0]}.uppercase"
+puts 
+
 
 end
 
